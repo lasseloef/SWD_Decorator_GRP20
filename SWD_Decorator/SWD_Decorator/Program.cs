7037d4c934f8106c4eec4a573e3f94ce287d5030
lasseloef <55458476+lasseloef@users.noreply.github.com>
2020-11-04 16:33:25 +0100

    Made VS solution

---
+++ SWD_Decorator/SWD_Decorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWD_Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
